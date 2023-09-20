13670fa44b65c827ed0f8224d0a69da893c4ee97
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-09-20 16:36:33 -0300

    Add files via upload

---
+++ organizer.py
@@ -0,0 +1,48 @@
+# Script file from https://medium.com/@gamsahamidah95/organize-files-in-folders-using-python-script-49fdd4afc6d5
+
+import os 
+import shutil 
+
+print("Starting Organize Tool....")
+
+# your directory
+desktop_path = "~/Downloads/"
+
+folders = {
+    "Images": [".jpeg", ".jpg", ".png", ".gif", ".jpeg", ".jpg", ".svg", ".bitmap"],
+    "Documents": [".doc", ".docx", ".pdf", ".txt",".xlsx", ".odt", ".odp", ".ods", ".PDF", ".csv", ".ppt", ".pptx"],
+    "Archives": [".zip", ".rar", ".dmg", ".iso", ".bz2", ".gz", ".7z"],
+    "Videos": [".MP4",".mp4",".mov", ".mkv", ".srt", ".avi"],
+    "Programming": [".sql",".py",".java", ".dia", ".json", ".php", ".html", ".js", ".css", ".sh", ".dtd"],
+    "Progs": [".flatpak",".deb",".exe", ".AppImage", ".flatpakref"],
+    "Torrents": [".torrent"]
+}
+
+for folder_name in folders:
+    folder_path = os.path.join(desktop_path, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        # print("n existe")
+    # else:
+    #     print("ja existe")
+        
+for file_name in os.listdir(desktop_path):
+    original_file_path = os.path.join(desktop_path, file_name)    
+    if os.path.isfile(original_file_path):
+        for folder_name, extensions in folders.items():
+            for extension in extensions:
+                if file_name.endswith(extension):
+                    destination_folder = os.path.join(desktop_path, folder_name)
+                    print(original_file_path)
+                    print(destination_folder)                
+                    shutil.move(original_file_path, destination_folder)
+                    try:
+                        os.unlink(original_file_path)
+                        # print("apagou")
+                    except:
+                        pass
+                        # print("deu ruim!")
+                        # exit()
+                    # exit()
+
+print("Done...")
