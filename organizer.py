f9838be6aedd2da253eaff9fce45ee31e794fbb7
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-20 15:46:08 -0300

    ok

--- organizer.py
+++ organizer.py
@@ -41,7 +41,7 @@ for file_name in os.listdir(desktop_path):
                         shutil.move(original_file_path, destination_folder)
                         os.remove(original_file_path)                        
                     except Exception as e:
-                        # se tiver arquivos com o mesmo nome, o arquivo das subpastas eh sobreescrito pelo novo arquivo que esta em Download
+                        # se tiver arquivos com o mesmo nome, o arquivo das subpastas eh sobreescrito pelo novo arquivo que esta(va) em desktop_path 
                         os.remove(destination_folder+"/"+file_name.strip())                        
                         shutil.move(original_file_path, destination_folder)
                         # os.remove(original_file_path)                     
